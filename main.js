a981025b895ed81d591c851b80db684c1b715fb4
Yargazi <94797678+Yargazi@users.noreply.github.com>
2022-06-23 22:03:38 +0300

    Milestone 3 - done

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
 const fiboText = document.getElementById("fibo_text");
-const fiboInput = document
-  .getElementById("fibo_Input")
-  .addEventListener("input", fibonacci);
-console.log(fiboInput);
-
+const fiboInput = document.getElementById("fibo_Input");
+const fiboBtn = document.getElementById("btn_fibo");
 let x, y;
-let fibo = [0, 1];
+
+
+const checkInput = () => fiboBtn.addEventListener("click", getValueFibo);  
+fiboInput.addEventListener('keydown', checkInput)
+
+const getValueFibo = x => {
+  x = fiboInput.value;
+  fibonacci(x);
+  return x;
+}
+
 function fibonacci(x) {
+  let fibo = [0, 1];
   if (x === 1) {
-    y = [1];
-    fiboText.innerHTML = `The Fibonacci of ${x} is ${y}`;
+    y = 1;
+    fiboText.innerHTML = y;
     return y;
   } else if (x === 0) {
-    y = [0];
-    fiboText.innerHTML = `The Fibonacci of ${x} is ${y}`;
+    y = 0;
+    fiboText.innerHTML = y;
     return y;
   }
   for (let i = 2; i <= x; i++) {
     fibo.push(fibo[i - 2] + fibo[i - 1]);
   }
-  y = fibo.slice(-1);
+  y = fibo.slice(-1)[0];
 
-  fiboText.innerHTML = `The Fibonacci of ${x} is ${y}`;
+  fiboText.innerHTML = y;
   return y;
 }
-
-// fibonacci(x);
