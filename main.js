b4ae5beafbe3fc9a3e8592d26475f90025d59fbf
Yargazi <94797678+Yargazi@users.noreply.github.com>
2022-06-24 00:04:21 +0300

    Milestone 4 - done

--- main.js
+++ main.js
@@ -1,34 +1,32 @@
+
 const fiboText = document.getElementById("fibo_text");
 const fiboInput = document.getElementById("fibo_Input");
 const fiboBtn = document.getElementById("btn_fibo");
+const url = `http://localhost:5050/fibonacci/${fiboInput.value}`
 let x, y;
 
 
 const checkInput = () => fiboBtn.addEventListener("click", getValueFibo);  
-fiboInput.addEventListener('keydown', checkInput)
 
-const getValueFibo = x => {
-  x = fiboInput.value;
-  fibonacci(x);
-  return x;
+const getValueFibo = url => {
+  url = `http://localhost:5050/fibonacci/${fiboInput.value}`
+  fibonacci(url);
+  return url;
 }
 
-function fibonacci(x) {
-  let fibo = [0, 1];
-  if (x === 1) {
-    y = 1;
-    fiboText.innerHTML = y;
-    return y;
-  } else if (x === 0) {
-    y = 0;
-    fiboText.innerHTML = y;
-    return y;
-  }
-  for (let i = 2; i <= x; i++) {
-    fibo.push(fibo[i - 2] + fibo[i - 1]);
-  }
-  y = fibo.slice(-1)[0];
+function fibonacci(url) {
+  try {
+  fetch(url).then((response) => {
+   response.json().then((data) => {
+     return fiboText.innerHTML = data.result 
+    })   
+  })
+} 
+catch(err) {
+  console.error(err)
+};
+    
+} 
 
-  fiboText.innerHTML = y;
-  return y;
-}
+
+fiboInput.addEventListener('keydown', checkInput);
