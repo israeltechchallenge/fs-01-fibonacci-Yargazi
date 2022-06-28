9ddd607fd1667bec55c62cf8380192a545c691da
Yargazi <94797678+Yargazi@users.noreply.github.com>
2022-06-28 15:36:44 +0300

    fixed server err in Milesone_5

--- main.js
+++ main.js
@@ -13,12 +13,14 @@ const checkInput = () => fiboBtn.addEventListener("click", getLinkFibo);
 const fibonacci = url => (fiboInput.value > 50) ? fibo_value_fifty(url) : fibo_fetch(url);
 
 const getLinkFibo = (url) => {
-
-  url = `http://localhost:5050/fibonacci/${fiboInput.value}`;
-  fibonacci(url);
-  return url;
-
+ 
+   url = `http://localhost:5050/fibonacci/${fiboInput.value}`
+   fibonacci(url);
+   return url;
+ 
 }
+   
+
 
 const fibo_value_fifty = url => {
 
@@ -37,28 +39,39 @@ const fibo_fetch = (url) => {
   loader.hidden = false;
   fiboInput.classList.remove('inputBorderColore')
 
-  fetch(url).then((response) => {
-   response.json().then((data) => {
-    errBlock.classList.add('visually-hidden');
-    errServer.hidden = true;
-    loader.hidden = false;
-    loader.hidden = true;
-    return fiboText.innerHTML = data.result; 
+  fetch(url)
+    .then((response) => {
+      if (response.ok) 
+      return response.json()
+      else {
+        return response.text()
+        .then((text) => {
+          throw new Error(text);
+        });
+        }
+      })
+        .then((data) => {
+        errBlock.classList.add('visually-hidden');
+        errServer.hidden = true;
+        loader.hidden = false;
+        loader.hidden = true;
+          return fiboText.innerHTML = data.result; 
+        }) 
+        .catch((err) => meaningOfLife(err));
+  };    
+      
+      
+    
+    
 
-    }) 
-    .catch((err) => meaningOfLife());
 
-  })
-  
-};
-
-function meaningOfLife() {
+function meaningOfLife(err) {
 
   loader.hidden = false;
   loader.hidden = true;
   errBlock.classList.add('visually-hidden');
   errServer.hidden = false;
-  errServer.innerText = 'Server Error: 42 is the meaning of life' 
+  errServer.innerText = `Server error: ${err.message}`; 
  return fiboText.innerText = (' ') 
 
 };
