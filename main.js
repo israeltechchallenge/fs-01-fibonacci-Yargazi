e071540c880a921bc7c1e3575fe3ee83a1113d48
Yargazi <94797678+Yargazi@users.noreply.github.com>
2022-07-03 01:58:42 +0300

    Milestone 6

--- main.js
+++ main.js
@@ -4,25 +4,37 @@ const fiboText = document.getElementById("fibo_text");
 const fiboBtn = document.getElementById("btn_fibo");
 const errBlock = document.getElementById('error-block-id');
 const errServer = document.getElementById('ifErrorInServ');
+const resultList = document.getElementById('resultList') 
+const resLoader = document.getElementById('resultsLoader')
+let li = document. createElement("li");
 let x, y, url;
 
 loader.hidden = true;
+resLoader.hidden = true;
+
 fiboInput.classList.remove('inputBorderColore');
 
 const checkInput = () => fiboBtn.addEventListener("click", getLinkFibo);  
-const fibonacci = url => (fiboInput.value > 50) ? fibo_value_fifty(url) : fibo_fetch(url);
 
-const getLinkFibo = (url) => {
+// if statement of Fibonacci
+
+const fibonacci = url => {
+  (fiboInput.value > 50) ? fiboValueFifty(url) : fiboFetch(url);
+  
+}
+
+// url fibonacci and cb
+const getLinkFibo = url => {
  
    url = `http://localhost:5050/fibonacci/${fiboInput.value}`
    fibonacci(url);
-   return url;
+  return url;
  
 }
-   
 
+// function if value > 50
 
-const fibo_value_fifty = url => {
+const fiboValueFifty = url => {
 
   loader.hidden = false;
     loader.hidden = true;
@@ -33,8 +45,10 @@ const fibo_value_fifty = url => {
     return fiboText.innerHTML = (' ');
 
 }
-      
-const fibo_fetch = (url) => {
+
+// function Fibonacci from server 
+
+const fiboFetch = url => {
 
   loader.hidden = false;
   fiboInput.classList.remove('inputBorderColore')
@@ -50,20 +64,19 @@ const fibo_fetch = (url) => {
         });
         }
       })
-        .then((data) => {
+    .then((data) => {
         errBlock.classList.add('visually-hidden');
         errServer.hidden = true;
         loader.hidden = false;
         loader.hidden = true;
-          return fiboText.innerHTML = data.result; 
+        getFiboList()  
+        return fiboText.innerHTML = data.result; 
+          
         }) 
         .catch((err) => meaningOfLife(err));
   };    
-      
-      
-    
-    
 
+  // Error Fucntion
 
 function meaningOfLife(err) {
 
@@ -76,5 +89,42 @@ function meaningOfLife(err) {
 
 };
 
+// url of Fibonacci results and cb
 
+const getFiboList = url => {
+url = 'http://localhost:5050/getFibonacciResults ';
+fetchDataFromServer(url);
+resLoader.hidden = false;
+return url;
+};
+
+
+// Milestone 6
+
+const fetchDataFromServer = url => {
+  
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    
+    data.results.forEach((elements) => {
+      const liResult = `<li class="fs-5-text border-bottom  border-secondary mt-4">The Fibonnaci Of <b>${elements.number}</b> is <b>${elements.result}</b>. Calculated at: ${(new Date(elements.createdDate)).toString()} <br> </li>`
+      resultList.insertAdjacentHTML('beforebegin', liResult);
+      
+      // I tried to display only 3 elements, but something is wrong.
+      if (data.results.length >= 3) {
+        let array = data.results.splice(0, 2);
+        return array;
+      }
+      resultList.insertAdjacentHTML('beforebegin', array);
+    })   
+    resLoader.hidden = true;
+    
+  })
+  
+}
+// cb function getFiboList on loading.
+window.addEventListener('load', getFiboList)
+
+// Input listener
 fiboInput.addEventListener('keydown', checkInput);
